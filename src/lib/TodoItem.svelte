d91a2aad0dd30569372a15cc2876cd29eb117e17
CaoXoa <thamdee2552@gmail.com>
2025-04-30 14:58:12 +0700

    GroupPlan

---
+++ src/lib/TodoItem.svelte
@@ -0,0 +1,66 @@
+<script>
+    let { task, index, deleteTask, saveTasks } = $props();
+</script>
+<li class={task.completed ? "completed" : ""}>
+    <div><input
+        type="checkbox"class="w-[15px] h-[15px]"
+        bind:checked={task.completed}
+        onclick={() => {
+            task.completed = !task.completed;
+            saveTasks();
+        }}
+    /></div>
+    <div><p>{task.text}</p></div>
+   <div class="del"> <button
+        class="delete-btn"
+        onclick={() => {
+            deleteTask(index);
+        }}>
+        <img src="image.png" alt="">
+        </button
+    ></div>
+</li> 
+<style>
+    .del{
+        display: flex;
+        justify-content: end;
+    }
+    li {
+        background-color: #B9CFFF;
+        height: 200px;
+        width: 200px;
+        padding: 8px;
+        margin-top: 40px;
+        border-radius: 5px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+
+    }
+
+    li p {
+        font-size: 25px;
+        flex-grow: 1;
+        margin: 0;
+        margin-left: 10px;
+        flex-wrap: wrap;
+    }
+
+    li.completed p {
+        opacity: 70%;
+    }
+
+    .delete-btn {
+        color: white;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+        margin-left: 10px;
+        height: 25px;
+        width: 25px;
+    }
+
+    .delete-btn:hover {
+        opacity: 90%;
+    }
+</style> 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,113 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import { onMount } from "svelte";
+    import TodoItem from "$lib/TodoItem.svelte";
+
+    onMount(() => {
+        tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    });
+
+    let tasks = $state([]);
+    let taskInput = $state("");
+
+    const addTask = () => {
+        if (taskInput !== "") {
+            tasks.push({ text: taskInput, completed: false });
+            taskInput = "";
+            saveTasks();
+        }
+    };
+
+    const deleteTask = (index) => {
+        tasks.splice(index, 1);
+        saveTasks();
+    };
+
+    const saveTasks = () => {
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    };
+</script>
+
+<div class="app">
+    <div class="container">
+        <h1 class="head">Group Plan</h1>
+        <ul id="taskList">
+            {#each tasks as task, index}
+                <TodoItem {task} {index} {deleteTask} {saveTasks} />
+            {/each}
+        </ul>
+
+        <div class="position">
+            <input
+                type="text"
+                placeholder="Add Task ..."
+                bind:value={taskInput}
+            />
+            <div class="flex flex-col justify-center w-[40px] h-[40px]">
+                <button id="addTaskBtn" onclick={addTask}><img src="add.png" alt=""></button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+    .head {
+        font-weight: bold;
+        font-size: larger;
+        display: flex;
+        justify-content: center;
+    }
+    .position {
+        display: flex;
+        justify-content: center;
+        margin-top: 20px;
+    }
+    .app {
+        font-family: Arial, sans-serif;
+        background-color: #f1f1f1;
+        display: flex;
+        padding: 50px;
+        height: 100vh;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .container {
+        background-color: white;
+        height: auto;
+        min-height: auto;
+        width: 4000px;
+        padding: 20px 30px;
+        border-radius: 10px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        display: flex;
+        flex-direction: column;
+    }
+    input[type="text"] {
+        width: 300px;
+        padding: 8px;
+        margin-bottom: 10px;
+        border: 2px solid #ccc;
+        border-radius: 50px;
+    }
+
+    #addTaskBtn {
+        width: 35px;
+        height: 35px;
+        margin-left: 5px;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+
+    #addTaskBtn:hover {
+        opacity: 90%;
+    }
+
+    ul {
+        list-style: none;
+        padding: 0;
+        display: grid;
+        align-items: center;
+        grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
+        grid-template-rows: auto;
+    }
+</style>
